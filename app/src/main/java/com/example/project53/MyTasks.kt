3206d02ae53bae2a0dcec2ed086722f32a260d7e
derek <derekalonso13@gmail.com>
2020-12-07 05:58:49 -0500

    tasks listview also working, same data problem 2nd

--- app/src/main/java/com/example/project53/MyTasks.kt
+++ app/src/main/java/com/example/project53/MyTasks.kt
@@ -2,25 +2,93 @@ package com.example.project53
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ListView
 import android.widget.Toast
-
+import com.example.project53.CreateJob.Companion.TAG
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.lang.Exception
+import java.util.*
 
 
 class MyTasks : Activity() {
 
-    private var numTasks = 0
+    private var numJobs = 0
     private lateinit var username: String
+    internal lateinit var jobsCreated: MutableList<String>
+    internal lateinit var listView: ListView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.mytasks)
+        setContentView(R.layout.mainlist)
+        listView = findViewById<ListView>(R.id.mainlist_listview)
+        jobsCreated = ArrayList()
 
         username = intent.getStringExtra("username").toString()
 
         // TODO - IMPLEMENT THIS CLASS
-        // TODO   Search all jobs currently in DB and display all jobs with current user as tasker
+        // TODO   Search all jobs currently in DB and display all jobs created by the current user
 
+        if (numJobs == 0) Toast.makeText(applicationContext, "No Jobs Currently", Toast.LENGTH_SHORT).show()
 
-        if (numTasks == 0) Toast.makeText(applicationContext, "No Tasks Currently", Toast.LENGTH_SHORT).show()
+    }
+    override fun onStart() {
+        super.onStart()
+        var mUsers = FirebaseDatabase.getInstance().getReference("Users").child(username!!)
+        var mUsers1 = FirebaseDatabase.getInstance().getReference("Jobs")
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                var userData = snapshot.getValue(User::class.java)
+                username = userData!!.name
+                jobsCreated = userData!!.tasksAccepted
+
+                val mAdapter = MyJobsAdapter(this@MyTasks, jobsCreated!!,username!!)
+                listView.adapter = mAdapter;
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        mUsers.addValueEventListener(postListener)/*
+        var dueDate: MutableList<String>? = ArrayList()
+        var payout: MutableList<String>? = ArrayList()
+        var location: MutableList<String>? = ArrayList()
+        var job: Job? = null
+        val postListener1 = object: ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onDataChange(snapshot1: DataSnapshot) {
+                var i = 0;
+
+                for (postSnap in snapshot1.children) {
+
+                    try {
+                        job = postSnap.getValue(Job::class.java)
+
+                    } catch (e: Exception) {
+                    } finally {
+                        if(job!!.jid == jobsCreated[i]){
+                            dueDate!!.add(job!!.date.toString())
+                            payout!!.add(job!!.payout.toString())
+                            location!!.add(job!!.zip.toString())
+                            i++
+                        }
+                    }
+                }
+            }
+        }
+        mUsers1.addValueEventListener(postListener1)
+        val mAdapter = MyJobsAdapter(this@MyJobs, jobsCreated!!, location!!, dueDate!!, payout!!)
+        listView.adapter = mAdapter;*/
     }
 }
--- app/src/main/java/com/example/project53/MyTasksAdapter.kt
+++ app/src/main/java/com/example/project53/MyTasksAdapter.kt
@@ -1,2 +1,50 @@
 package com.example.project53
 
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import java.util.*
+
+class MyTasksAdapter(private val mContext: Context, tasks: MutableList<String>, userName: String) : BaseAdapter(){
+    private var mytasks = tasks
+    private var name = userName
+
+    override fun getItem(pos: Int): Any {
+
+        return mytasks[pos]
+
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return mytasks.size
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        val curTask = mytasks[position]
+
+
+
+        var mLayoutInflater : LayoutInflater = LayoutInflater.from(mContext)
+        val rowView = mLayoutInflater.inflate(R.layout.list_item,null,true)
+        val descView = rowView.findViewById(R.id.listitem_desc) as TextView
+        val dateView = rowView.findViewById(R.id.listitem_date) as TextView
+        val timeView = rowView.findViewById(R.id.listitem_location) as TextView
+        val payoutView = rowView.findViewById(R.id.listitem_dollar) as TextView
+        val createdView = rowView.findViewById(R.id.listitem_username) as TextView
+
+        descView.text = curTask
+        createdView.text = name
+
+        return rowView
+    }
+
+
+
+}
